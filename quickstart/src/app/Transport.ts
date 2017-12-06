f259713d2234a307f107137ba5b3e37db11abea9
Enault-de Courval Louis <1358447@cegepmontpetit.ca>
2017-12-06 13:49:57 -0500

    Model changes

--- quickstart/src/app/Transport.ts
+++ quickstart/src/app/Transport.ts
@@ -3,3 +3,10 @@ export class Transport{
     Id:number;
     Prix:number;
 }
+
+export const TRANSPORTS: Transport[] = [
+    { Id:1,Prix:20},
+    { Id:2,Prix:100},
+    { Id:3,Prix:15},
+    { Id:4,Prix:30}
+]
