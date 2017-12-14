9d86ec7bcebc6220bc12d6bbc7eafa86e40f4fab
Pelletier Guillaume <1551579@cegepmontpetit.ca>
2017-12-14 17:50:39 -0500

    t

--- quickstart/src/app/Voyage.ts
+++ quickstart/src/app/Voyage.ts
@@ -1,14 +1,10 @@
 import {Destination, DESTINATIONS} from "./Destination";
 
-export class Voyage{
+export class Voyage {
     Id:number;
     Nom:String;
     Destinations:Destination[];
     Budget:number;
     NumberOfDays:number;
     Cost:number;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> f66e712ab3caae1b2831b66c804d3fff71a0b055
