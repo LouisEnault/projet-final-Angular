31ad6688384512e06c8ba4eb6483705bfe8e98f6
Pelletier Guillaume <1551579@cegepmontpetit.ca>
2017-12-14 17:37:23 -0500

    Merge branch 'master' of https://github.com/LouisEnault/projet-final-Angular
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- quickstart/src/app/Voyage.ts
+++ quickstart/src/app/Voyage.ts
@@ -8,8 +8,3 @@ export class Voyage{
     NumberOfDays:number;
     Cost:number;
 }
-
-export const VOYAGES: Voyage[] = [
-    { Id:1, Budget : 2000, Destinations :[ DESTINATIONS[0],DESTINATIONS[1]], Nom :"super voyage 1", NumberOfDays : 10},
-    { Id:2, Budget : 1500,Destinations :[ DESTINATIONS[2],DESTINATIONS[3]], Nom :"super voyage 2", NumberOfDays : 14}
-    ]
