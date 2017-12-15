71c7c7134d64de559ab82980c6deee0ae54f3fbe
McDuff Benjamin <1074966@cegepmontpetit.ca>
2017-12-15 13:13:45 -0500

    se mettre a jour est dure

--- quickstart/src/app/Jour.ts
+++ quickstart/src/app/Jour.ts
@@ -1,15 +1,9 @@
 import {Activite, ACTIVITES} from "./Activite";
 import {Destination} from "./Destination";
 
-export class Jour{
+export class Jour {
     Id:number;
     Activites:Activite[];
-<<<<<<< HEAD
+    Destination:string;
+    Cout:number;
 }
-
-
-=======
-	Destination:Destination;
-	Cout:number;
-}
->>>>>>> 57a0b16754cfe83b900248eed7b5a8066c65d171
