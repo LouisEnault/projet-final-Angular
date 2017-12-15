16f33ddbfc26b51ac8ca38ec3652e610bbadd9de
thebotz <jduhaime96@gmail.com>
2017-12-15 13:10:42 -0500

    Activite service

--- quickstart/src/app/Jour.ts
+++ quickstart/src/app/Jour.ts
@@ -4,12 +4,6 @@ import {Destination} from "./Destination";
 export class Jour{
     Id:number;
     Activites:Activite[];
-<<<<<<< HEAD
-}
-
-
-=======
 	Destination:Destination;
 	Cout:number;
 }
->>>>>>> 57a0b16754cfe83b900248eed7b5a8066c65d171
